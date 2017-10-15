3aaec724517aa03a0d0dbdbf3287eeec68e7121a
nicholi <nschell@gmail.com>
2017-10-14 18:07:42 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const http = require('http');
+const https = require('https');
+
+const Express = require('express');
+const ExpressWs = require('express-ws');
+
+let isSecure = false;
+
+let app = Express();
+
+let httpServer;
+if (isSecure) {
+  let httpsOptions = {};
+  httpServer = https.createServer(httpsOptions, app);
+} else {
+  httpServer = http.createServer(app);
+}
+
+function verifyWsClient (info, cb) {
+  console.log('Received upgrade request');
+  cb(true, null, null);
+  return;
+}
+
+let expressWsOptions = {
+  wsOptions: {
+    verifyClient: verifyWsClient 
+  }
+};
+let expressWsResult = ExpressWs(app, httpServer, expressWsOptions);
+
+let wss = expressWsResult.getWss(); 
+
+app.get('/app', (req, res) => {
+  console.log('Received http request');
+  res.send('Hello World');
+});
+
+app.ws('/ws', (ws, req) => {
+  console.log('Received ws connection');
+  ws.on('message', (msg) => {
+    console.log('Received ws request');
+    ws.send('Goodbye World');
+  });
+});
+
+app.use(Express.static('public'));
+
+httpServer.listen(process.env['PORT'], () => {
+  console.log('Listening for connections');
+});
+
